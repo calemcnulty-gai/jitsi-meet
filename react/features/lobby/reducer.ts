calemcnulty-gai/jitsi-meet

commit 732e5e38c398be25356b265ba423ea1f426f2ee9

fix: Clear up knocking state on error.
Fixes a case where participant is not entering lobby when trying the second time after being rejected.

react/features/lobby/reducer.ts
@@ -60,7 +60,10 @@ ReducerRegistry.register<ILobbyState>('features/lobby', (state = DEFAULT_STATE, 
             };
         }
 
-        return state;
+        return {
+            ...state,
+            knocking: false
+        };
     }
     case CONFERENCE_JOINED:
     case CONFERENCE_LEFT:

commit cf6da8300e79f7d7e4d781a23f81dfe395a6e460

fix(lobby): Fixes wrong password going back to knocking.
Fixes the case when someone enters a wrong password to access the meeting and then clicks back to continue knocking and not showing the knocking state, while still in the Lobby room.
The problem was introduced in 732e5e3, on access denied we are being kicked out of lobby room and then knocking state should be cleared.

react/features/lobby/reducer.ts
@@ -58,12 +58,14 @@ ReducerRegistry.register<ILobbyState>('features/lobby', (state = DEFAULT_STATE, 
                 ...state,
                 isDisplayNameRequiredError: true
             };
+        } else if (action.error.name === JitsiConferenceErrors.CONFERENCE_ACCESS_DENIED) {
+            return {
+                ...state,
+                knocking: false
+            };
         }
 
-        return {
-            ...state,
-            knocking: false
-        };
+        return state;
     }
     case CONFERENCE_JOINED:
     case CONFERENCE_LEFT: