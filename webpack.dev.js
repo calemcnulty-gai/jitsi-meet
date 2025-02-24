d5f02b5aa834b477d5e7d209a0a4b63156e7a5eb
Cale McNulty <cale.mcnulty@gauntletai.com>
2025-02-24 10:56:18 -0600

    dev build

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,20 @@
+const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.config.js');
 
-// Override the dev server configuration to listen on all interfaces
-const devConfig = baseConfig.getDevServerConfig();
-devConfig.host = '0.0.0.0';
-devConfig.allowedHosts = 'all';  // Allow all hosts to connect
-
-module.exports = {
-    ...baseConfig,
-    devServer: devConfig
-}; 
+module.exports = merge(baseConfig, {
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080,
+        allowedHosts: 'all',
+        client: {
+            overlay: {
+                errors: true,
+                warnings: false
+            }
+        },
+        hot: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+}); 
